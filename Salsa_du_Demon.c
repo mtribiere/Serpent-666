1406dd452e411f30b165f55d4964c78ee8483641
MaxEnerGyzz <35264654+MaxEnerGyzz@users.noreply.github.com>
2020-12-09 14:29:55 +0100

    Add files via upload

---
+++ Salsa_du_Demon.c
@@ -0,0 +1,10 @@
+
+
+void couplet_salsa_du_demon(int couplet) {
+	switch(couplet) {
+		case 0: printf("Horreur, malheur\nOui, je suis Belzébuth (horreur)\nJe suis un bouc, je suis en rut (horreur, malheur)\nOui, oui, oui, je vis dans l'ordure (horreur)\nJe pue la sueur et la luxure\nJe fume, je bois, j'ai tous les vices\nEt j'ai du poil partout sur les cuisses\nJe vous déteste, je vous maudis\nJ'suis complètement pourri\nQuand j'vois un gosse, j'lui fous une claque\nQuand j'vois une vieille j'lui pique son sac\nJe crache, je rote, rien ne m'arrête\nCar aujourd'hui c'est la fête\nC'est la, c'est la, c'est la\nSalsa du démon\nSalsa du démon\nSalsa du démon\nSalsa du démon\n\n"); break;
+		case 1: printf("Horreur, malheur\nOui, c'est moi Vampirella (horreur)\nMalheur à ceux qui ne m'aiment pas (horreur, malheur)\nOui, oui, oui, mon cœur est en fer (horreur)\nJe fais l'amour comme une panthère\nMes amants, je les écorche vifs\nEt je les fouette, je leur coupe le pif\nJ'fais des trucs cochons avec des chaînes\nAux minets du 16ème\nPourchassant les puceaux en fuite\nLe démon du sexe m'habite\nVenez là mes petits amis\nCar c'est la fête aujourd'hui\nC'est la, c'est la, c'est la\nSalsa du démon\nSalsa du démon\nSalsa du démon\nSalsa du démon\n\n"); break;
+		case 2: printf("Horreur, malheur\nOui, je suis la sorcière (horreur)\nJ'suis vieille, j'suis moche, j'suis une mégère (horreur, malheur)\nOui, oui, oui, sur mon balai maudit (horreur)\nJ'aime bien faire mal aux tous petits\nJe fais bouillir des mains de pendus\nJ'mange des crapauds, des rats tout poilus\nJ'fais des potions pour séduire les hommes\nPuis j'les mords quand ils dorment\nDans ma marmite c'est l'épouvante\nY a des bestioles dégoulinantes\nCe soir j'fais du bœuf au pipi\nCar c'est la fête aujourd'hui\nC'est la, c'est la, c'est la\nSalsa du démon\nSalsa du démon\nSalsa du démon\nSalsa du démon\n\n"); break;
+
+}
+}
